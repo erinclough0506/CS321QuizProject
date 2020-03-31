3d69f137e6bc3352dca5d503c99f81ed51292886
Erin Clough <enc0006@uah.edu>
2020-03-31 12:50:55 -0500

    flashcardform

--- src/FlashCardForm.java
+++ src/FlashCardForm.java
@@ -1,25 +1,30 @@
-public class FlashCardForm {
+public class FlashCardForm
+{
+    //Similar to QuestionForm, this class acts as a
+    // //template for creating flashcards. It
+    //is responsible for getting user input
+    // and sending the user created flashcards to be stored.
 
 
 
+    public void CreateFlashcard()
+    {
+        //create new flash card object
+        String prompt;
+        String answer;
 
+        public Flashcard(String prompt, String answer)
+        {
+            this.prompt = prompt;
+            this.answer = answer;
 
-
-
-    public void CreateFlashcard(){
-
-
+        }
 
 
     }
 
 
 
-    public void Flashcard(){
-
-
-
-    }
 
 
 }
