adbb29e192e6db050b1ebdaa5c018659f12acf10
aww0008 <aww0008@uah.edu>
2020-04-06 14:38:13 -0500

    Building XML True False Doc

--- src/main/java/True_False.java
+++ src/main/java/True_False.java
@@ -6,48 +6,109 @@ import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 
 import org.w3c.dom.NodeList;
 
 
 public class True_False {
+    public static void main(String[] args){
 
 
-    String prompt;
-    String answer;
-    String correct_answer;
-    String incorrect_answer;
-    int correct = 0;
-    int incorrect = 0;
-    boolean answerB = false;
+        //----------------------------------------
+        //Temporary driver
+        //----------------------------------------
+    True_False Item = new True_False();
+    Scanner myObj = new Scanner(System.in);
+        System.out.println("Write your question");
+
+    String Question = myObj.nextLine();
 
 
-    True_False[] QuestionB = new True_False[10];
+        System.out.println("Set Whether is is true or false");
+    Scanner myObj2 = new Scanner(System.in);
+    String Flag = myObj2.nextLine();
+
+        System.out.println("The question is " + Question);
+        System.out.println("The Flag is set to  " + Flag);
+
+//--------------------------------------------------------------------
 
 
-    public String SetQuestion(String obj) {
 
-        String Question = " ";
+    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+    DocumentBuilder dBuilder;
+    try{
+        dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.newDocument();
+        // add elements to Document
+        Element rootElement = doc.createElement("User");
+        //append root element to document
+        doc.appendChild(rootElement);
 
-        return Question;
 
+        //This shows that input variables can be used to fill out the xml  document.
+        //append first child element to root element
+        rootElement.appendChild(createUserElement(doc, "1", Question, Flag));
+
+        //append first child element to root element
+        rootElement.appendChild(createUserElement(doc, "2", Question, Flag));
+
+        //for output to file, console
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        //for pp
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        DOMSource source = new DOMSource(doc);
+        //For formatting
+        StreamResult console = new StreamResult(System.out);
+        StreamResult file = new StreamResult(new File("create_user.xml"));
+
+        //Write Data
+        transformer.transform(source, console);
+        transformer.transform(source, file);
+
+    } catch(Exception e)
+
+    {
+
+        e.printStackTrace();
     }
 
-    public Boolean SetFlag(String obj) {
-        Boolean Flag = true;
+}
 
-        if (obj == "true") {
+    //-----------------------------------
+//Creates the elements tags.
+//-----------------------------------
+    private static Node createUserElement(Document doc,String Question_num,String Question,String Flag )
+    {
+        Element user = doc.createElement("User");
+        //set id attribute
+        user.setAttribute("Question_num",Question_num);
 
-            Flag = true;
-        }
-        if (obj == " false") {
-            Flag = false;
-        }
+        //create Question
+        user.appendChild(createUserElements(doc,user,"Question",Question));
+        //Create Flag
+        user.appendChild(createUserElements(doc,user,"Flag",Flag));
 
-        return Flag;
-
+        return user;
     }
+//will need to write a utility method to create the text node
+
+
+    // utility method to create text node
+    private static Node createUserElements(Document doc, Element element, String name, String value) {
+        Element node = doc.createElement(name);
+        node.appendChild(doc.createTextNode(value));
+        return node;
+    }
+
+
 
     public void ReadQuestions() {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
