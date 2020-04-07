1abb9d5e35a692d460179d9383ab44ba715c363f
aww0008 <aww0008@uah.edu>
2020-04-07 14:37:45 -0500

    Building XML True False Doc

--- src/main/java/True_False.java
+++ src/main/java/True_False.java
@@ -1,96 +1,104 @@
-import javax.xml.parsers.DocumentBuilderFactory;
-import java.util.*;
-import java.util.Arrays;
-import java.io.*;
-import org.w3c.dom.*;
-import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.Transformer;
 import javax.xml.transform.stream.StreamResult;
-
-
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
 
+import javax.xml.transform.dom.DOMSource;
 
-public class True_False {
-    public static void main(String[] args){
+public class CreateXML {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+
 
 
         //----------------------------------------
         //Temporary driver
         //----------------------------------------
-    True_False Item = new True_False();
-    Scanner myObj = new Scanner(System.in);
+
+        True_False Item = new True_False();
+        Scanner myObj = new Scanner(System.in);
         System.out.println("Write your question");
 
-    String Question = myObj.nextLine();
+        String Question = myObj.nextLine();
 
 
         System.out.println("Set Whether is is true or false");
-    Scanner myObj2 = new Scanner(System.in);
-    String Flag = myObj2.nextLine();
+        Scanner myObj2 = new Scanner(System.in);
+        String Flag = myObj2.nextLine();
 
         System.out.println("The question is " + Question);
         System.out.println("The Flag is set to  " + Flag);
 
-//--------------------------------------------------------------------
+
+//---------------------------------------------------------------------
 
 
 
-    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-    DocumentBuilder dBuilder;
-    try{
-        dBuilder = dbFactory.newDocumentBuilder();
-        Document doc = dBuilder.newDocument();
-        // add elements to Document
-        Element rootElement = doc.createElement("User");
-        //append root element to document
-        doc.appendChild(rootElement);
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder;
+        try{
+            dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.newDocument();
+            // add elements to Document
+            Element rootElement = doc.createElement("User");
+            //append root element to document
+            doc.appendChild(rootElement);
 
 
-        //This shows that input variables can be used to fill out the xml  document.
-        //append first child element to root element
-        rootElement.appendChild(createUserElement(doc, "1", Question, Flag));
+            //This shows that input variables can be used to fill out the xml  document.
+            //append first child element to root element
+            rootElement.appendChild(createUserElement(doc, "1","1", Question, Flag));
 
-        //append first child element to root element
-        rootElement.appendChild(createUserElement(doc, "2", Question, Flag));
+            //append first child element to root element
+            rootElement.appendChild(createUserElement(doc, "2","2", Question, Flag));
 
-        //for output to file, console
-        TransformerFactory transformerFactory = TransformerFactory.newInstance();
-        Transformer transformer = transformerFactory.newTransformer();
-        //for pp
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        DOMSource source = new DOMSource(doc);
-        //For formatting
-        StreamResult console = new StreamResult(System.out);
-        StreamResult file = new StreamResult(new File("create_user.xml"));
+            //for output to file, console
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            //for pp
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            DOMSource source = new DOMSource(doc);
+            //For formatting
+            StreamResult console = new StreamResult(System.out);
+            StreamResult file = new StreamResult(new File("create_user.xml"));
 
-        //Write Data
-        transformer.transform(source, console);
-        transformer.transform(source, file);
+            //Write Data
+            transformer.transform(source, console);
+            transformer.transform(source, file);
+            getQuestion(doc);
 
-    } catch(Exception e)
 
-    {
+        } catch(Exception e)
 
-        e.printStackTrace();
+        {
+
+            e.printStackTrace();
+        }
+
     }
-
-}
 
     //-----------------------------------
 //Creates the elements tags.
 //-----------------------------------
-    private static Node createUserElement(Document doc,String Question_num,String Question,String Flag )
+    private static Node createUserElement(Document doc,String Question_num,String Num,String Question,String Flag)
     {
-        Element user = doc.createElement("User");
+        Element user = doc.createElement("QuestionNum");
         //set id attribute
-        user.setAttribute("Question_num",Question_num);
+        user.setAttribute("Users",Question_num);
 
+        //set number
+        user.appendChild(createUserElements(doc,user,"num",Num));
         //create Question
         user.appendChild(createUserElements(doc,user,"Question",Question));
         //Create Flag
@@ -109,23 +117,25 @@ public class True_False {
     }
 
 
+    private static void getQuestion(Document doc)
+    {
+        NodeList QuestionList = doc.getElementsByTagName("QuestionNum");
+        for(int i=0; i<QuestionList.getLength(); i++)
+        {
+            Node QuestionNode = QuestionList.item(i);
+            if(QuestionNode.getNodeType() == Node.ELEMENT_NODE)
+            {
+                Element QuestionElement = (Element) QuestionNode;
+                String QuestionId = QuestionElement.getElementsByTagName("num").item(0).getTextContent();
+                String QuestionName = QuestionElement.getElementsByTagName("Question").item(0).getTextContent();
+                String QuestionFlag = QuestionElement.getElementsByTagName("Flag").item(0).getTextContent();
+                System.out.println("Question Number = " + QuestionId);
+                System.out.println("Question = " + QuestionName);
+                System.out.println("Question Flag = " + QuestionFlag);
 
-    public void ReadQuestions() {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        try {
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder.parse("Items");
-            NodeList personList = doc.getElementsByTagName("Items");
-
-            personList.getLength();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        } catch (SAXException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            }
         }
     }
-}//---------------------------------------------
 
+}
 
