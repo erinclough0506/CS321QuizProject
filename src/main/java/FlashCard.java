92788f7c50add11d5a157dd1f123976aa48136bd
Erin Clough <enc0006@uah.edu>
2020-04-13 15:37:57 -0500

    flashcard class

--- src/main/java/FlashCard.java
+++ src/main/java/FlashCard.java
@@ -1,11 +1,30 @@
 public class FlashCard
 {
+    int num = 1;
+    String term = null;
+    String definition = null;
 
-
-
-
-    public void StoreQuestion(){
+    public FlashCard(int num, String term, String definition)
+    {
+        this.num = num;
+        this.term = term;
+        this.definition = definition;
 
     }
 
+    public String getTerm() {
+        return term;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
 }
